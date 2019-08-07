ee89af66aa0080cbf79b59c9c906d322bdcf2ae7
Steven Rigolet <steven-r@hotmail.fr>
2019-08-07 15:37:52 +0200

    exercice okay but witouh front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "creating restaurants ..."
+
+les_bouteilles = Restaurant.new(name: "Les bouteilles", address: "talensac",
+  phone_number: "0102030405", category: "french")
+les_bouteilles.save!
+
+
+paola = Restaurant.new(name: "Paola", address: "Ile de Nantes",
+  phone_number: "0102030405", category: "italian")
+paola.save!
+
+melting_pote = Restaurant.new(name: "Melting Pote", address: "Ile de Nantes",
+  phone_number: "0102030405", category: "french")
+melting_pote.save!
+
+
+bonhommie = Restaurant.new(name: "Bonhommie", address: "Paris",
+  phone_number: "0102030405", category: "french")
+bonhommie.save!
+
+
+okuda = Restaurant.new(name: "Okuda", address: "Paris",
+  phone_number: "0102030405", category: "japanese")
+okuda.save!
+
+puts "ok !"
